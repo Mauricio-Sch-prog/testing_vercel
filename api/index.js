26d1955f02c8647164d6a0280ef43c2d7286b022
Programador_Gaúcho <mauricio.ass.2016@gmail.com>
2026-04-01 19:46:48 -0300

    login

--- api/index.js
+++ api/index.js
@@ -3,24 +3,63 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'path';
 import {configDotenv} from "dotenv";
+import cookieParser from "cookie-parser";
 
 configDotenv()
 
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(cookieParser());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 app.set('views', path.join(__dirname, '..', 'view'));
 
 app.get('/', (req, res) => {
+
+  if(!req.cookies?.["auth"]) return res.redirect("/login")
+
   res.render('index', {
     titulo: 'Sistema Teste',
     mensagem: 'Aplicacao rodando',
     data: new Date().toLocaleString('pt-BR')
   });
 });
+
+app.get('/login', (req, res) => {
+  
+  
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  
+
+  res.render('register');
+});
+
+
+app.post('/login', (req, res) => {
+  const {name, email, password} = req.body;
+
+  const data = {}
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  res.cookie("auth", data, options).redirect('/');
+});
+
+app.post('/register', (req, res) => {
+  const {email, password} = req.body;
+
+  res.redirect('/login');
+});
+
+
 
 if (process.env.NODE_ENV !== 'production') {
   const port = process.env.PORT || 3000;
